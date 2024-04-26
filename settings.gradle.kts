4e3d8e553100b227cdde77252690da0f3c8cf60e
Won SeungBin <wsb0514@naver.com>
2024-04-26 22:47:10 +0900

    [SETTING] Clean Architecture를 위한 멀티 모듈 생성

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,7 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    includeBuild("build-logic")
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
@@ -15,4 +16,11 @@ dependencyResolutionManagement {
 
 rootProject.name = "UnSplash"
 include(":app")
- 
+include(":feature:home")
+include(":domain:entity")
+include(":domain:repository")
+include(":domain:usecase")
+include(":data:local")
+include(":data:remote")
+include(":data:model")
+include(":data:repository")
