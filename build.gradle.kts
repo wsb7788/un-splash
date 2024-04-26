fcd5e2e552110d57610de1b74f96956bcab5d378
Won SeungBin <wsb0514@naver.com>
2024-04-26 20:19:59 +0900

    [SETTING] dependency 항목 추가 및 project plugin 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,8 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.hilt.android) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.android.library) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
