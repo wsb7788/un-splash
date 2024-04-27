954d6fc2ac12c3ee11c7be0d387229d0e932ab7c
Won SeungBin <wsb0514@naver.com>
2024-04-27 16:43:10 +0900

    [FEAT] BottomNavigation Icon, select interaction color 적용

--- feature/home/src/main/java/com/guesthouse/home/HomeRoute.kt
+++ feature/home/src/main/java/com/guesthouse/home/HomeRoute.kt
@@ -4,14 +4,24 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.guesthouse.navigation.HomeGraphRoute
 
@@ -20,11 +30,15 @@ fun HomeRoute(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
 ) {
+    val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
     Scaffold(
         bottomBar = {
-            BottomNavigationBar(navController = navController) { route ->
-                navController.navigate(route)
-            }
+            BottomNavigationBar(
+                currentRoute = currentRoute,
+                onNavigationItemClicked = { route ->
+                    navController.navigate(route)
+                },
+            )
         },
     ) {
         Box(
@@ -42,19 +56,48 @@ fun HomeRoute(
 
 @Composable
 fun BottomNavigationBar(
-    navController: NavHostController,
+    currentRoute: String?,
     modifier: Modifier = Modifier,
     onNavigationItemClicked: (String) -> Unit,
 ) {
     NavigationBar(
-        modifier = modifier.height(100.dp),
+        modifier = modifier,
     ) {
         HomeGraphRoute.entries.forEach {
             NavigationBarItem(
-                selected =  false ,
-                onClick = { onNavigationItemClicked(it.name) },
-                icon = { },
-                label = { Text(text = it.name) }
+                selected =  it.name == currentRoute ,
+                onClick = {
+                    onNavigationItemClicked(it.name)
+                          },
+                icon = {
+                       when(it.name){
+                           HomeGraphRoute.FEED.name -> {
+                               Icon(
+                                   imageVector = Icons.Default.Add,
+                                   contentDescription = stringResource(
+                                       R.string.feed_bottom_navigation
+                                    ),
+                               )
+                           }
+                           HomeGraphRoute.FAVORITES.name -> {
+                               Icon(
+                                   imageVector = Icons.Default.Favorite,
+                                   contentDescription = stringResource(
+                                       R.string.favorites_bottom_navigation
+                                   ),
+                               )
+                           }
+                       }
+                },
+                label = {
+                    Text(text = it.name)
+                },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = Color.Blue,
+                    selectedTextColor = Color.Blue,
+                    unselectedIconColor = Color.LightGray,
+                    unselectedTextColor = Color.LightGray,
+                ),
                 )
         }
     }
