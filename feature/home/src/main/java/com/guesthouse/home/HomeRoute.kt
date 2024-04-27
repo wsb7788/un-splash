8c19e44bf0d4431d192bf4df4a8fe484f1a64215
Won SeungBin <wsb0514@naver.com>
2024-04-27 16:44:38 +0900

    [REFACTOR] 코드 정리

--- feature/home/src/main/java/com/guesthouse/home/HomeRoute.kt
+++ feature/home/src/main/java/com/guesthouse/home/HomeRoute.kt
@@ -2,16 +2,13 @@ package com.guesthouse.home
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.NavigationBarItemColors
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -19,7 +16,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
@@ -51,7 +47,6 @@ fun HomeRoute(
             )
         }
     }
-
 }
 
 @Composable
@@ -65,29 +60,30 @@ fun BottomNavigationBar(
     ) {
         HomeGraphRoute.entries.forEach {
             NavigationBarItem(
-                selected =  it.name == currentRoute ,
+                selected = it.name == currentRoute,
                 onClick = {
                     onNavigationItemClicked(it.name)
-                          },
+                },
                 icon = {
-                       when(it.name){
-                           HomeGraphRoute.FEED.name -> {
-                               Icon(
-                                   imageVector = Icons.Default.Add,
-                                   contentDescription = stringResource(
-                                       R.string.feed_bottom_navigation
-                                    ),
-                               )
-                           }
-                           HomeGraphRoute.FAVORITES.name -> {
-                               Icon(
-                                   imageVector = Icons.Default.Favorite,
-                                   contentDescription = stringResource(
-                                       R.string.favorites_bottom_navigation
-                                   ),
-                               )
-                           }
-                       }
+                    when (it.name) {
+                        HomeGraphRoute.FEED.name -> {
+                            Icon(
+                                imageVector = Icons.Default.Add,
+                                contentDescription = stringResource(
+                                    R.string.feed_bottom_navigation
+                                ),
+                            )
+                        }
+
+                        HomeGraphRoute.FAVORITES.name -> {
+                            Icon(
+                                imageVector = Icons.Default.Favorite,
+                                contentDescription = stringResource(
+                                    R.string.favorites_bottom_navigation
+                                ),
+                            )
+                        }
+                    }
                 },
                 label = {
                     Text(text = it.name)
@@ -98,8 +94,7 @@ fun BottomNavigationBar(
                     unselectedIconColor = Color.LightGray,
                     unselectedTextColor = Color.LightGray,
                 ),
-                )
+            )
         }
     }
-
 }
