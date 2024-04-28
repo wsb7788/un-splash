f9c618ab0fa9ad43cce952385eddaea8ac6e59f8
Won SeungBin <wsb0514@naver.com>
2024-04-29 02:49:45 +0900

    [FEAT] photo 좋아요 버튼 추가

--- feature/home/src/main/java/com/guesthouse/home/feed/FeedScreen.kt
+++ feature/home/src/main/java/com/guesthouse/home/feed/FeedScreen.kt
@@ -1,16 +1,27 @@
 package com.guesthouse.home.feed
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.VectorPainter
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -19,6 +30,7 @@ import androidx.paging.compose.collectAsLazyPagingItems
 import coil.compose.AsyncImage
 import com.guesthouse.designsystem.component.UnSplashSearchBar
 import com.guesthouse.entity.Photo
+import com.guesthouse.home.R
 import kotlinx.coroutines.flow.flow
 
 @OptIn(ExperimentalComposeUiApi::class)
@@ -53,7 +65,8 @@ internal fun FeedScreen(
     ) {
 
         LazyVerticalStaggeredGrid(
-            modifier = Modifier.padding(top = it.calculateTopPadding() + 5.dp),
+            modifier = Modifier
+                .padding(top = it.calculateTopPadding() + 5.dp),
             columns = StaggeredGridCells.Fixed(3),
             verticalItemSpacing = 3.dp,
             horizontalArrangement = Arrangement.spacedBy(3.dp)
@@ -61,7 +74,9 @@ internal fun FeedScreen(
             items(
                 items = photos.itemSnapshotList.items,
             ) { photo ->
-                PhotoItem(photo)
+                PhotoItem(
+                    photo = photo,
+                    )
             }
         }
 
@@ -70,10 +85,31 @@ internal fun FeedScreen(
 
 @Composable
 fun PhotoItem(photo: Photo) {
+
     AsyncImage(
         modifier = Modifier
             .fillMaxWidth(),
         model = photo.url,
         contentDescription = null
     )
+    Box(
+        modifier = Modifier.padding(10.dp),
+        contentAlignment = Alignment.TopEnd
+    ) {
+        if (photo.likedByUser) {
+            Icon(
+                imageVector = Icons.Default.Favorite,
+                contentDescription = "",
+                tint = Color.Red
+            )
+        } else {
+            Icon(
+                imageVector = Icons.Default.FavoriteBorder,
+                contentDescription = "",
+            )
+        }
+
+    }
+
+
 }
