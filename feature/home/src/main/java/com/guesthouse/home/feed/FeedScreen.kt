334428e0b8f94a57c33d07cf30a7e8d9d8b3078c
Won SeungBin <wsb0514@naver.com>
2024-04-29 03:14:02 +0900

    [DESIGN] PhotoItem 구조 및 click event 범위 수정

--- feature/home/src/main/java/com/guesthouse/home/feed/FeedScreen.kt
+++ feature/home/src/main/java/com/guesthouse/home/feed/FeedScreen.kt
@@ -1,37 +1,25 @@
 package com.guesthouse.home.feed
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.graphics.vector.VectorPainter
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
-import coil.compose.AsyncImage
 import com.guesthouse.designsystem.component.PhotoItem
 import com.guesthouse.designsystem.component.UnSplashSearchBar
 import com.guesthouse.entity.Photo
-import com.guesthouse.home.R
 import kotlinx.coroutines.flow.flow
 
 @OptIn(ExperimentalComposeUiApi::class)
@@ -63,26 +51,39 @@ internal fun FeedScreen(
                 keyboardController = keyboardController,
             )
         }
-    ) {
+    ) { paddingValues ->
 
-        LazyVerticalStaggeredGrid(
-            modifier = Modifier
-                .padding(top = it.calculateTopPadding() + 5.dp),
-            columns = StaggeredGridCells.Fixed(3),
-            verticalItemSpacing = 3.dp,
-            horizontalArrangement = Arrangement.spacedBy(3.dp)
-        ) {
-            items(
-                items = photos.itemSnapshotList.items,
-            ) { photo ->
-                PhotoItem(
-                    photoUrl = photo.url,
-                    isLiked = photo.likedByUser,
-                    onPhotoClicked = {viewModel.onPhotoClicked( photo.id, photo.likedByUser) }
-                    )
-            }
-        }
+
+        LazyGridPhotoList(
+            paddingValues = paddingValues,
+            photos = photos,
+            onPhotoClicked = viewModel::onPhotoClicked
+        )
 
     }
 }
+
+@Composable
+private fun LazyGridPhotoList(
+    paddingValues: PaddingValues,
+    photos: LazyPagingItems<Photo>,
+    onPhotoClicked: (String, Boolean) -> Unit
+) {
+    LazyVerticalStaggeredGrid(
+        modifier = Modifier
+            .padding(top = paddingValues.calculateTopPadding() + 5.dp),
+        columns = StaggeredGridCells.Fixed(3),
+        verticalItemSpacing = 3.dp,
+        horizontalArrangement = Arrangement.spacedBy(3.dp)
+    ) {
+        items(
+            items = photos.itemSnapshotList.items,
+        ) { photo ->
+            PhotoItem(
+                photoUrl = photo.url,
+                isLiked = photo.likedByUser,
+                onPhotoClicked = { onPhotoClicked(photo.id, photo.likedByUser) }
+            )
+        }
+    }
 }
